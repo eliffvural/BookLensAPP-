301a6a8db8a49078128b1efa8bfb1e9861abb2a4
Elif Vural <elifvural019@gmail.com>
2024-10-13 09:46:33 +0300

    implementation added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    val nav_version="2.7.7"
+
+    implementation("androidx.navigation:navigation-fragment-ktx: $nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx: $nav_version")
 }
