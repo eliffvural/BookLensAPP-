c4269aeb90b5e312c515abddaec469043dc6c11c
Elif Vural <elifvural019@gmail.com>
2024-10-13 10:15:40 +0300

    first fragment added with nav

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,6 @@ dependencies {
 
     val nav_version="2.7.7"
 
-    implementation("androidx.navigation:navigation-fragment-ktx: $nav_version")
-    implementation("androidx.navigation:navigation-ui-ktx: $nav_version")
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 }
